cbe7ee1547252ecfdaee24b37c991a744c6a025a
frolui <frolui29@gmail.com>
2021-03-13 22:52:05 +0300

    feat: add show and hide content functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const startButton = querySelector('.start-button');
+    const startButton = document.querySelector('.start-button'),
+          actors = document.querySelectorAll('.actor');
 
-    startButton.addEventListener('click', (event) =>{
+    startButton.addEventListener('click', (e) =>{
         e.preventDefault();
+
+        startButton.remove();
+        setInterval(showContent, 2000);
     });
 
+    function hideContent() {
+        actors.forEach(item =>{
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+    }
 
-}
+    function showContent() {
+        actors.forEach(item =>{
+            item.classList.add('show');
+            item.classList.remove('hide', 'fade');
+        });
+    }
+
+
+});
