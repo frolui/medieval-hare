0c4fcc96effdabff419879e929ef655ec27ef41e
frolui <frolui29@gmail.com>
2021-03-20 17:00:07 +0300

    refactor: change switch styles

--- js/script.js
+++ js/script.js
@@ -6,8 +6,9 @@ window.addEventListener('DOMContentLoaded', () => {
           boxTwo = document.querySelector('.box-2'),
           newQuestions = document.querySelector('.new-questions'),
           btnSlice = document.querySelector('.btn-slice'),
+          footerImg = document.querySelector('.a-footer'),
           collapse = document.querySelectorAll('.div-wrapper'),
-          arrItems = [question, boxTwo, btnSlice, newQuestions],
+          arrItems = [question, boxTwo, btnSlice, newQuestions, footerImg],
           openAnswerButton = document.querySelector('.collapse-processed').firstElementChild;
 
     startButton.addEventListener('click', (e) =>{
@@ -45,22 +46,30 @@ window.addEventListener('DOMContentLoaded', () => {
             collapse[0].classList.add('show', 'active');
             collapse[0].classList.remove('hide');
         }        
-    });
+    });    
 
-    newQuestions.addEventListener('click', (e) =>{
-        e.preventDefault(); 
-        
-        fetch('https://db.chgk.info/random')
-            .then(response => response.text())
-            .then(data => console.log(data));
-
-        
-    });
-    
-        
-
+    // newQuestions.addEventListener('click', (e) =>{
+    //     e.preventDefault();    
+    //     fetch('https://engine.lifeis.porn/api/millionaire.php?qType=1&count=1')
+    //         .then(response => response.text())
+    //         .then(data => console.log(data));       
+    // });     
     // const parser = new DOMParser();
     // let doc = parser.parseFromString(stringContainingHTMLSource, "text/html");
+    
+    footerImg.addEventListener('click', (e) =>{
+        e.preventDefault();
 
+        if (footerImg.classList.contains('active')) {            
+            setInterval(hideContent(arrItems), 0);
+            footerImg.classList.remove('active');
+        } else {
+            setInterval(showContent(arrItems), 0);
+            footerImg.classList.add('active');
+        }
+
+        footerImg.classList.add('show'); 
+        footerImg.classList.remove('hide');
+    });
 
 });
